7bdbd85e16949e2c92a622f1bbc453c3a698de00
Lior Tamam <leo212@gmail.com>
2019-04-16 20:43:15 +0300

    added longestPath method that finds the longest path size starting a specific point. need to extend it so it will return the whole path

--- src/app/route-generation-algorithm/route.generation.algorithm.service.ts
+++ src/app/route-generation-algorithm/route.generation.algorithm.service.ts
@@ -7,17 +7,34 @@ import {Point} from "../models/point.model";
 import {Route} from "../models/route.model";
 import {main} from "@angular/compiler-cli/src/main";
 import {el} from "@angular/platform-browser/testing/src/browser_util";
-import Any = jasmine.Any;
 
 interface IRoute {
   weight: number;
   routeNodes: number[];
 }
 
+class AdjListNode {
+  constructor(private v: number, private weight: number) {
+    this.v = v;
+    this.weight = weight;
+  }
+  v: number;
+  weight: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class RouteGenerationAlgorithmService {
+  constructor(private dataService: DataService, private store: Store<any>) {
+    this.graph = [[], []];
+    store.select('aircraft').subscribe((aircraft: Map<number, Aircraft>) => {
+      this.aircrafts = Array.from(aircraft.values());
+    });
+    store.select('points').subscribe((points: Map<number, Point>) => {
+      this.pointsMap = points;
+    });
+  }
 
   private mapPointToIndex = new Map();
 
@@ -37,14 +54,10 @@ export class RouteGenerationAlgorithmService {
 
   private routesMap: Map<number, Route> = new Map();
 
-  constructor(private dataService: DataService, private store: Store<any>) {
-    this.graph = [[], []];
-    store.select("aircraft").subscribe((aircraft: Map<number, Aircraft>) => {
-      this.aircrafts = Array.from(aircraft.values());
-    });
-    store.select("points").subscribe((points: Map<number, Point>) => {
-      this.pointsMap = points;
-    });
+  private edges: Array<Array<AdjListNode>>;
+
+  private static generateRandomColor(): string {
+    return Math.floor(Math.random() * 16777215).toString(16);
   }
 
   // algorithm:
@@ -54,23 +67,41 @@ export class RouteGenerationAlgorithmService {
   //      2. find the maximal weight route
   //      3. delete edges, who found at level before
   // 3. write result to file
-public generate_routes(): string {
-    let routes: IRoute[] = [];
+  public generate_routes(): string {
+    const routes: IRoute[] = [];
     this.buildMapPointIdToIndex();
     this.buildMapIndexToPointId();
-    let numberOfPoint: number = this.mapPointIdToIndex.size;
+    const numberOfPoint: number = this.mapPointIdToIndex.size;
+
+    this.edges = new Array<Array<AdjListNode>>(6);
+    for (let i = 0; i < 6; i++) {
+      this.edges[i] = new Array<AdjListNode>();
+    }
+    this.addEdge(0, 1, 5);
+    this.addEdge(0, 2, 3);
+    this.addEdge(1, 3, 6);
+    this.addEdge(1, 2, 2);
+    this.addEdge(2, 4, 4);
+    this.addEdge(2, 5, 2);
+    this.addEdge(2, 3, 7);
+    this.addEdge(3, 5, 1);
+    this.addEdge(3, 4, -1);
+    this.addEdge(4, 5, -2);
+
+    this.longestPath(1, 6);
+
     this.graph = new Array(numberOfPoint).fill(0).map(() => new Array(numberOfPoint).fill(0));
     this.buildGraph();
     while (this.haveEdge(this.graph)) {
       // don't ask why i use the temp value but its not work
       // if i send the function, like: this.findSrc(this.findMaxEdge());
-      let [x, y, z] = this.findMaxEdge();
-      let src = this.findSrc([x, y, z]);
-      let dest = this.findDest([x, y, z]);
+      const [x, y, z] = this.findMaxEdge();
+      const src = this.findSrc([x, y, z]);
+      const dest = this.findDest([x, y, z]);
       if (src == dest) {
         break;
       }
-      let maximalRoute = this.findMaximalRoute1(this.graph, src, dest);
+      const maximalRoute = this.findMaximalRoute1(this.graph, src, dest);
       routes.push(maximalRoute);
       this.removeMaximalRoute(this.graph, maximalRoute.routeNodes);
     }
@@ -83,12 +114,12 @@ public generate_routes(): string {
   private buildMapPointIdToIndex() {
     // iterate over all aircrafts
     for (let i = 0; i < this.aircrafts.length; i++) {
-      let currentAircraft = this.aircrafts[i];
-      let points = currentAircraft.path;
+      const currentAircraft = this.aircrafts[i];
+      const points = currentAircraft.path;
       // iterate over all points to build map
       for (let j = 0; j < points.length; j++) {
         if (!this.mapPointIdToIndex.has(points[j].pointId)) {
-          this.mapPointIdToIndex.set(points[j].pointId, j)
+          this.mapPointIdToIndex.set(points[j].pointId, j);
         }
       }
     }
@@ -97,13 +128,13 @@ public generate_routes(): string {
   private buildGraph() {
     // iterate over all aircrafts
     for (let i = 0; i < this.aircrafts.length - 1; i++) {
-      let currentAircraft = this.aircrafts[i];
-      let points = currentAircraft.path;
+      const currentAircraft = this.aircrafts[i];
+      const points = currentAircraft.path;
       for (let j = 0; j < points.length - 1; j++) {
-        let sourcePoint = points[j];
-        let sourcePointIndex = this.mapPointIdToIndex.get(sourcePoint.pointId);
-        let destPoint = points[j + 1];
-        let destPointIndex = this.mapPointIdToIndex.get(destPoint.pointId);
+        const sourcePoint = points[j];
+        const sourcePointIndex = this.mapPointIdToIndex.get(sourcePoint.pointId);
+        const destPoint = points[j + 1];
+        const destPointIndex = this.mapPointIdToIndex.get(destPoint.pointId);
         this.graph[sourcePointIndex][destPointIndex]++;
       }
     }
@@ -113,7 +144,7 @@ public generate_routes(): string {
   // srcMaxEdge and check if there is a edge with the same weight
   // if found run again with the new srcMaxEdge
   // currently, 08/04/2019 we assume there is no circle
-  //this.findMaxEdge();
+  // this.findMaxEdge();
   /*
   1   2   3   4   5
 
@@ -125,16 +156,15 @@ public generate_routes(): string {
 
   5
    */
-  private findSrc([srcMaxEdge, destMaxEdge, maxWeight],deviationInPercent = 5 ): number {
-    let candidateSrc = srcMaxEdge;
+  private findSrc([srcMaxEdge, destMaxEdge, maxWeight], deviationInPercent = 5): number {
+    const candidateSrc = srcMaxEdge;
     for (let i = 0; i < this.graph[srcMaxEdge].length; i++) {
-      let currentColumn = this.getColumn(this.graph, srcMaxEdge);
+      const currentColumn = this.getColumn(this.graph, srcMaxEdge);
       if (i != destMaxEdge && maxWeight == currentColumn[i]) {
         return this.findSrc([i, srcMaxEdge, maxWeight]);
-      }
-      else{
+      } else {
         //  check for more edges with deviation of number's plan
-        if(i != destMaxEdge && this.isBetween(maxWeight, currentColumn[i])){
+        if (i != destMaxEdge && this.isBetween(maxWeight, currentColumn[i])) {
           return this.findSrc([i, srcMaxEdge, maxWeight]);
         }
       }
@@ -142,9 +172,9 @@ public generate_routes(): string {
     return candidateSrc;
   }
 
-  private isBetween(weight: number, value:number, deviationInPercent: number = 6.5):boolean{
-    return ((weight < value && Math.ceil(weight + weight * deviationInPercent /100) > value)  ||
-      (weight > value && Math.floor( weight - weight * deviationInPercent /100) <value))
+  private isBetween(weight: number, value: number, deviationInPercent: number = 6.5): boolean {
+    return ((weight < value && Math.ceil(weight + weight * deviationInPercent / 100) > value) ||
+      (weight > value && Math.floor(weight - weight * deviationInPercent / 100) < value));
   }
 
   /*
@@ -158,17 +188,17 @@ public generate_routes(): string {
 
   5
    */
+
   // the opposite of the findSrc
   private findDest([srcMaxEdge, destMaxEdge, maxWeight]): number {
-    let candidateDest = destMaxEdge;
-    //assume this.graph[srcMaxEdge] bring the column
+    const candidateDest = destMaxEdge;
+    // assume this.graph[srcMaxEdge] bring the column
     for (let i = 0; i < this.graph[destMaxEdge].length; i++) {
-      let currentRow = this.graph[destMaxEdge];
+      const currentRow = this.graph[destMaxEdge];
       if (i != destMaxEdge && maxWeight == currentRow[i]) {
         return this.findDest([destMaxEdge, i, maxWeight]);
-      }
-      else{
-        if(i != destMaxEdge && this.isBetween(maxWeight, currentRow[i])){
+      } else {
+        if (i != destMaxEdge && this.isBetween(maxWeight, currentRow[i])) {
           return this.findDest([i, srcMaxEdge, maxWeight]);
         }
       }
@@ -177,8 +207,8 @@ public generate_routes(): string {
   }
 
   private getColumn(matrix, col: number) {
-    var column = [];
-    for (var i = 0; i < matrix.length; i++) {
+    const column = [];
+    for (let i = 0; i < matrix.length; i++) {
       column.push(matrix[i][col]);
     }
     return column;
@@ -196,7 +226,7 @@ public generate_routes(): string {
         if (this.graph[i][j] > maxWeight) {
           maxWeight = this.graph[i][j];
           maxI = i;
-          maxJ = j
+          maxJ = j;
         }
       }
     }
@@ -233,7 +263,7 @@ public generate_routes(): string {
 
       /* Add the weight of i-dest to the weight up to i, and see
          whether the total weight is bigger than the current max value */
-      let maxWeightCompare = maxRoute.weight + graph[i][dest];
+      const maxWeightCompare = maxRoute.weight + graph[i][dest];
       if (maxWeightCompare > maxWeightCurrent) {
         maxWeightCurrent = maxWeightCompare;    // Save the new max
         // Save the route with the maximum value (from src up to dest)
@@ -249,15 +279,15 @@ public generate_routes(): string {
 
   private findMaximalRoute1(graph: number[][],
                             src: number, dest: number): IRoute {
-    let maxWeightCurrent = 0;       // The current maximal route's total
-    let maxWeightCurrentNodes = []; // The nodes for maxWeightCurrent
+    const maxWeightCurrent = 0;       // The current maximal route's total
+    const maxWeightCurrentNodes = []; // The nodes for maxWeightCurrent
     let maxRoute: IRoute;           // The maximal route to return
     if (!maxRoute) {
       maxRoute = {weight: 0, routeNodes: [src]};
     }
 
     while (src != dest) {
-      let currentRow = graph[src];
+      const currentRow = graph[src];
       let maxRowWeight = 0;
       let maxIndex = 0;
 
@@ -302,22 +332,18 @@ public generate_routes(): string {
 
   }
 
-  private static generateRandomColor(): string {
-    return Math.floor(Math.random() * 16777215).toString(16);
-  }
-
 
   private updateRoutes(routes: IRoute[]) {
     for (let i = 0; i < routes.length; i++) {
-      let route = this.buildRoute(routes[i], i);
+      const route = this.buildRoute(routes[i], i);
       this.routesMap.set(i, route);
     }
-    let s = 5;
+    const s = 5;
   }
 
   private buildRoute(route: IRoute, routeId: number,
-                     primaryTextColor: string = "ffffff", secondaryTextColor: string = "187aab", visible: boolean = true): Route {
-    let currentRoute: Route = new Route();
+                     primaryTextColor: string = 'ffffff', secondaryTextColor: string = '187aab', visible: boolean = true): Route {
+    const currentRoute: Route = new Route();
     currentRoute.routeId = routeId;
     currentRoute.color = RouteGenerationAlgorithmService.generateRandomColor();
     currentRoute.primaryTextColor = primaryTextColor;
@@ -328,9 +354,9 @@ public generate_routes(): string {
   }
 
   private convertPointsIdToPoints(pointsIdsAsIndex: number[]): Point[] {
-    let result: Point[] = [];
+    const result: Point[] = [];
     pointsIdsAsIndex.forEach(pointIdAsIndex => {
-      result.push(this.mapIndexToPoint.get(pointIdAsIndex))
+      result.push(this.mapIndexToPoint.get(pointIdAsIndex));
     });
     return result;
   }
@@ -339,5 +365,81 @@ public generate_routes(): string {
     this.mapPointIdToIndex.forEach((value: number, key: number) => {
       this.mapIndexToPoint.set(value, this.pointsMap.get(key));
     });
+  }
+
+  private addEdge(u: number, v: number, weight: number) {
+    const node: AdjListNode = new AdjListNode(v, weight);
+    this.edges[u].push(node);
+  }
+
+  private topologicalSortUtil(v: number, visited: boolean[], stack: number[]) {
+    // Mark the current node as visited
+    visited[v] = true;
+
+    // Recur for all the vertices adjacent to this vertex
+    for (const node of this.edges[v]) {
+      if (!visited[node.v]) {
+        this.topologicalSortUtil(node.v, visited, stack);
+      }
+    }
+
+    // Push current vertex to stack which stores topological
+    // sort
+    stack.push(v);
+  }
+
+  private longestPath(startPoint: number, numOfPoints: number) {
+    const NINF = -9999999;
+    const stack: number[] = [];
+    const dist: number[] = [];
+    const visited: boolean[] = [];
+
+    //  Mark all the vertices as not visited
+    for (let i = 0; i < numOfPoints; i++) {
+      visited[i] = false;
+    }
+
+    // Call the recursive helper function to store Topological
+    // Sort starting from all vertices one by one
+    for (let i = 0; i < numOfPoints; i++) {
+      if (visited[i] === false) {
+        this.topologicalSortUtil(i, visited, stack);
+      }
+    }
+
+    // Initialize distances to all vertices as infinite and
+    // distance to source as 0
+    for (let i = 0; i < numOfPoints; i++) {
+      dist[i] = NINF;
+    }
+    dist[startPoint] = 0;
+
+    // Process vertices in topological order
+    while (stack.length > 0) {
+      /* Get the next vertex from topological order*/
+      const u: number = stack.pop();
+
+      // Update distances of all adjacent vertices
+      if (dist[u] !== NINF) {
+        for (const node of this.edges[u]) {
+          if (dist[node.v] < dist[u] + node.weight) {
+            dist[node.v] = dist[u] + node.weight;
+          }
+        }
+      }
+    }
+
+    // Print the calculated longest distances
+    let longestDistance = NINF;
+    let targetNode = startPoint;
+
+    for (let i = 0; i < numOfPoints; i++) {
+      if (dist[i] > longestDistance) {
+        targetNode = i;
+        longestDistance = dist[i];
+      }
+    }
+
+
   }
 }
