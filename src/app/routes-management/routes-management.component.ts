9e4719a1fcdd055665ff8ab8d6afba7547a8cf9d
nvdvrts <nvdvrts@gmail.com>
2021-04-10 13:48:19 +0300

    hotfix: add new route

--- src/app/routes-management/routes-management.component.ts
+++ src/app/routes-management/routes-management.component.ts
@@ -147,7 +147,7 @@ export class RoutesManagementComponent implements OnInit {
   }
 
   addRoute() {
-    let route = new Route(this.routes.length + 2, `מסלול ${this.routes.length + 2}`, "#FF0000");
+    let route = new Route(this.routes[this.routes.length-1].routeId + 1, `מסלול ${this.routes[this.routes.length-1].routeId + 1}`, "#FF0000");
     this.routes.push(route);
     this.updateRoute(route);
   }
